695ee36b9b7f4128e63e73df4c40a6b72534eb81
Yury <kotovyuriy27@gmail.com>
2022-06-13 02:09:24 +0300

    added socket connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,24 @@
 import express, { Express, Request, Response } from 'express';
+import http from 'http';
+import { Server } from "socket.io";
 
 const app: Express = express();
 const port = process.env.port || 3000;
 
+const server: http.Server = http.createServer(app);
+const io = new Server(server);
+
 // set cookie with unique user id and nickname
 // set cookie expiration
+// realize socketss
 
 app.get('/', (req, res: Response) => {
   res.send('enter nickname page should be there!')
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.get('/games', (req, res: Response) => {
   res.send('games page should be there!')
